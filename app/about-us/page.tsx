60b256839b07d6a562ee2bb9277eb61223421e3f
kimo21899 <lkm24@naver.com>
2025-03-30 07:25:47 +0900

    delete lottie adnimation

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,8 @@
-"use client"
-
 import styles from "./about-us.module.css";
-import Animation from "../components/animation";
 
-
+export const metadata = {
+  title: 'ABOUT US',
+}
 export default function AboutUs() {
   return (
     <div className={styles.container}>
@@ -11,12 +10,6 @@ export default function AboutUs() {
           <h1>
              About Us
           </h1>
-          <p>
-          
-          </p>
-        </div>
-        <div className={styles.animation_box}>
-            <Animation />
         </div>
     </div>
   )
