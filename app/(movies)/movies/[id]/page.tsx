4d78528a1de1fe5cf7725469f0584944219903c2
kimo21899 <lkm24@naver.com>
2025-03-29 22:41:15 +0900

    edit params{id}

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,19 +2,19 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
-
 // Dynamic Metadata
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata( { params }: { params: { id: string } }) {
+  const {id} = await params;
   const movie= await getMovie(id);
   return {
     title : movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+// params를 함수 인자로 받을 때 params: { id }처럼 직접 구조 분해 하지 않음.
+// 함수 내부에서 const { id } = params;로 구조 분해.
+export default async function MovieDetailPage( { params }: { params: { id: string } }) {
+  const {id} = await params;
   return (
     <div>
       <Suspense fallback={<h6>Loading movie info</h6>}>
