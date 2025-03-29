8aecdea6b52e8c467c19fb9f226525db700bdbf1
kimo21899 <lkm24@naver.com>
2025-03-29 22:53:54 +0900

    prams use Promise

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,19 +2,24 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
+// Next.js 15 이후, Params와 SearchParams는 이제 Promise를 사용해야 함
+type IParams = Promise<{
+  id: string;
+}>;
+
 // Dynamic Metadata
-export async function generateMetadata( { params }: { params: { id: string } }) {
-  const {id} = await params;
-  const movie= await getMovie(id);
+export async function generateMetadata(props: { params: IParams }) {
+  const params = await props.params;
+  const movie= await getMovie(params.id);
   return {
     title : movie.title,
   };
 }
 
-// params를 함수 인자로 받을 때 params: { id }처럼 직접 구조 분해 하지 않음.
-// 함수 내부에서 const { id } = params;로 구조 분해.
-export default async function MovieDetailPage( { params }: { params: { id: string } }) {
-  const {id} = await params;
+export default async function MovieDetailPage(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
+
   return (
     <div>
       <Suspense fallback={<h6>Loading movie info</h6>}>
