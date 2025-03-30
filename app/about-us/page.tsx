2d24c3304d0477af0266c648cb0d06dd225a480f
kimo21899 <lkm24@naver.com>
2025-03-30 15:30:21 +0900

    delete lottie

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,18 +3,15 @@ import styles from "./about-us.module.css";
 export const metadata = {
   title: 'ABOUT US',
 }
+
 export default function AboutUs() {
   return (
     <div className={styles.container}>
+        <h1>
+          About Us
+        </h1>
         <div className={styles.about_box}>
-          <h1>
-            About Us
-          </h1>
-        </div>
-        <div className="p-4">
-          <h1 className="w-full font-medium h-5 flex text-base">
-              Hello Tailwindcss
-          </h1>
+          <p>next.js 학습용 프로젝트입니다.</p>
         </div>
     </div>
   )
